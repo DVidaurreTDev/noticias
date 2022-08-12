bf295fccbfcc32e9c94599937dfafb7f8883e51b
DVidaurreTDev <dvidaurretdev@gmail.com>
2022-08-12 17:47:49 -0400

    dark-mode-v3

--- js/main.js
+++ js/main.js
@@ -13,4 +13,40 @@ const toggleDarkMode = () => {
     pItems.forEach(element => {
         element.classList.toggle("text-dark-mode")
     });
-}
+
+    let pCards = document.querySelectorAll(".card");
+    pCards.forEach(element => {
+        element.classList.toggle("card-dark-mode")
+    });
+
+    let pCardsA = document.querySelectorAll(".card a");
+    pCardsA.forEach(element => {
+        element.classList.toggle("text-dark-mode")
+    });
+
+    let pCardsS = document.querySelectorAll(".card span");
+    pCardsS.forEach(element => {
+        element.classList.toggle("text-dark-mode")
+    });
+
+    let pPageLink = document.querySelectorAll(".page-link");
+    pPageLink.forEach(element => {
+        element.classList.toggle("page-link-dark-mode")
+    });
+
+    let pPageItemA = document.querySelectorAll(".page-item a");
+    pPageItemA.forEach(element => {
+        element.classList.toggle("text-dark-mode")
+    });
+
+    let button = document.querySelector(".noticias-section-darkmode--btn");
+    if (button.innerHTML === "Dark Mode" ) {
+        button.innerHTML = "Light Mode";
+        button.style.backgroundColor = "white"
+        button.style.color = "black"
+    } else {
+        button.innerHTML = "Dark Mode";
+        button.style.backgroundColor = "black"
+        button.style.color = "white"
+    }
+    }
